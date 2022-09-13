341ed13eb902abbeaead8961b1ca1445805d8c5c
Kaodinna <chijiokenwoye64@gmail.com>
2022-09-13 16:11:21 +0100

    challenge _2_solution

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,23 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    {
+        let count = new Array(NO_OF_CHARS);
+        count.fill(0);
+          
+        for (let i = 0; i < str.length; i++)
+            count[str[i].charCodeAt()]++;
+             
+        for (let i = 0; i < NO_OF_CHARS; i++)
+        {
+            if(count[i] > 1)
+            {
+                document.write(String.fromCharCode(i) + ", " +
+                "count = " + count[i] + "</br>");
+            }
+        }    
+    }
+     
+    let str = "test string";
+      printDups(str);
+}
 
 module.exports = isolateDuplicates;
--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,10 @@
-function removeDuplicates(obj) {}
+function removeDuplicates(obj) {
+    const dataArr = [];
+
+const resultArr = dataArr.filter((data,index)=>{
+  return dataArr.indexOf(data) === index;
+})
+return(resultArr);
+}
 
 module.exports = removeDuplicates;
